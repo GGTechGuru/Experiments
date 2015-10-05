e350b96abdc38f56c42d320c0f2550346d4bd736
Gerard Anthony Gold <croesus@localhost.localdomain>
2015-10-05 07:32:53 -0300

    Nicer version of checkboxes

--- sinatra/L01/Checkboxes01.rb
+++ sinatra/L01/Checkboxes01.rb
@@ -5,9 +5,9 @@ set :port, 3012
 get '/' do
   <<-HTML
     <form method="post" action="/checked">
-      <input type="checkbox" name="check1" value="item 1" />
-      <input type="checkbox" name="check2" value="item 2" />
-      <input type="checkbox" name="check3" value="item 3" />
+      <input type="checkbox" name="check[]" value="item 1" />
+      <input type="checkbox" name="check[]" value="item 2" />
+      <input type="checkbox" name="check[]" value="item 3" />
       <input type="submit" value="Select"/>
     </form>
   HTML
